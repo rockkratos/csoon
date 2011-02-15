9389a721f1f37eb720451b6004d5e44aecf702d4
RockZhan <rockkratos@gmail.com>
2011-02-15 23:24:31 +0800

    init

---
+++ app/db/csoon_ddl.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `usr_info` (
+  `usr_id` varchar(32) NOT NULL,
+  `usr_name` varchar(32) NOT NULL,
+  `usr_pwd` varchar(6) NOT NULL,
+  PRIMARY KEY (`usr_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
---
+++ app/src/main/java/com/acs/csoon/mgr/ITestMgr.java
@@ -0,0 +1,10 @@
+package com.acs.csoon.mgr;
+
+import com.acs.csoon.vo.UsrInfo;
+
+public interface ITestMgr {
+
+	public void updateUsr(UsrInfo usr);
+	public void addUsr(UsrInfo usr);
+	
+}
